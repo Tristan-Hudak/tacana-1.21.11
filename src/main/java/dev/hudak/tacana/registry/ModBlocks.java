fb96e186e7713f9a32068f7047beecd16bee7360
Tristan-Hudak <tristanj.hudak@gmail.com>
2026-01-09 14:58:55 -0500

    Added registries for blocks, items, and creative tab

--- src/main/java/dev/hudak/tacana/registry/ModBlocks.java
+++ src/main/java/dev/hudak/tacana/registry/ModBlocks.java
@@ -1,5 +1,13 @@
 package dev.hudak.tacana.registry;
 
+import dev.hudak.tacana.Tacana;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModBlocks {
 
 }
--- src/main/java/dev/hudak/tacana/registry/ModItems.java
+++ src/main/java/dev/hudak/tacana/registry/ModItems.java
@@ -1,4 +1,10 @@
 package dev.hudak.tacana.registry;
 
+import dev.hudak.tacana.Tacana;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
 }
